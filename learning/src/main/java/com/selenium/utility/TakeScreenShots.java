d914ab8be1da6f4b9320450b66c3080415613499
M1027999 <M1027999@A1ML14663.mindtree.com>
2017-04-13 16:16:18 +0530

    Updated code

--- learning/src/main/java/com/selenium/utility/TakeScreenShots.java
+++ learning/src/main/java/com/selenium/utility/TakeScreenShots.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-public class TakeScreenShots {
+public class TakeScreenShots extends BaseClass{
 	
-public void captureScreenshot(WebDriver driver,String imageName) throws IOException{
+public void captureScreenshot(String imageName) throws IOException{
 	
 	TakesScreenshot ts=(TakesScreenshot)driver;
 	File source=ts.getScreenshotAs(OutputType.FILE);
--- learning/src/test/java/com/selenium/learning/TescCasesTest.java
+++ learning/src/test/java/com/selenium/learning/TescCasesTest.java
@@ -1,38 +1,15 @@
 package com.selenium.learning;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.selenium.pageObjects.RegistrationPage;
+import com.selenium.utility.BaseClass;
 
-public class TescCasesTest {
-	static WebDriver driver;
+public class TescCasesTest extends BaseClass {
 
-	/*public TescCasesTest( ) {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		driver = new ChromeDriver();
 
-	}
-*/
-	@BeforeTest
-	void openBrowser() {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
-		driver.get("http://way2automation.com/way2auto_jquery/index.php");
-		//driver.get("https://www.facebook.com/");
-
-		driver.findElement(
-				By.xpath("/html/body/div/div/table[2]/tbody/tr/td/table[1]/tbody/tr[2]/td/a"))
-				.click();
-
-	}
+	
 
 	@Test
 	void registration() {
